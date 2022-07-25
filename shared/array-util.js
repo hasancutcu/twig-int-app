a53c7c367bc824165946e3a7060db3c615e4fef5
Hasan Cutcu <hasancutcu@windowslive.com>
2022-07-25 14:51:25 +0100

    Result is okay

--- shared/array-util.js
+++ shared/array-util.js
@@ -1,6 +1,14 @@
-const groupArrayElements = (array, divider) => {
-  // const miniArraySize = Math.ceil(arr.)
-  // const result = 
+const groupArrayElements = (arr, divider) => {
+  const miniArraySize = Math.ceil(arr.length / divider);
+
+  const result = arr.reduce((prevVal, currArr, index) => {
+    if (index % miniArraySize === 0) {
+      prevVal.push([]);
+    }
+    prevVal[prevVal.length - 1].push(currArr);
+    return prevVal;
+  }, []);
+  return result;
 }
 
 module.exports = groupArrayElements;
