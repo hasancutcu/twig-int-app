64809a317522e5fba5cf4e85fbc43dbbb50a8b4c
Hasan Cutcu <hasancutcu@windowslive.com>
2022-07-25 14:57:23 +0100

    splitting reducer fucntion

--- shared/array-util.js
+++ shared/array-util.js
@@ -1,14 +1,17 @@
-const groupArrayElements = (arr, divider) => {
-  const miniArraySize = Math.ceil(arr.length / divider);
-
-  const result = arr.reduce((prevVal, currArr, index) => {
-    if (index % miniArraySize === 0) {
+const groupingReducerHOF = (miniArrLength) => {
+  const groupingReducer = (prevVal, currArr, index) => {
+    if (index % miniArrLength === 0) {
       prevVal.push([]);
     }
     prevVal[prevVal.length - 1].push(currArr);
     return prevVal;
-  }, []);
-  return result;
+  }
+  return groupingReducer;
+}
+
+const groupArrayElements = (arr, divider) => {
+  const miniArraySize = Math.ceil(arr.length / divider);
+  return arr.reduce(groupingReducerHOF(miniArraySize), []);
 }
 
 module.exports = groupArrayElements;
