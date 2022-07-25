47ea1c7f88a1ee00d7ac5b546ed8456f51c0f2ea
Hasan Cutcu <hasancutcu@windowslive.com>
2022-07-25 14:42:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const groupArrayElements = require('./shared/array-util');
+
+const result = groupArrayElements([1, 2, 3, 4, 5], 3);
+
+
+// should return this [ [ 1, 2 ], [ 3, 4 ], [ 5 ] ]
---
+++ shared/array-util.js
@@ -0,0 +1,7 @@
+const groupArrayElements = (array, divider) => {
+  // const miniArraySize = Math.ceil(arr.)
+  // const result = 
+}
+
+module.exports = groupArrayElements;
+
